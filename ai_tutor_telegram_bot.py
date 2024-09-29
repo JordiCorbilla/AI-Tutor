cbe408d8a80d59d30363ead49a7d6a1eb0029a6d
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2024-09-29 17:46:12 +0100

    Update ai_tutor_telegram_bot.py

--- ai_tutor_telegram_bot.py
+++ ai_tutor_telegram_bot.py
@@ -51,7 +51,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 # Set up logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
     handlers=[
         logging.FileHandler("bot.log"),
         logging.StreamHandler()
